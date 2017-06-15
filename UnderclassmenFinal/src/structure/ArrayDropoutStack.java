2fe31a5b90249a462703d89d7071c4c81b1f00a0
frankatank4526 <frankatank1@gmail.com>
2017-06-15 14:10:43 -0700

    Update ArrayDropoutStack.java

--- UnderclassmenFinal/src/structure/ArrayDropoutStack.java
+++ UnderclassmenFinal/src/structure/ArrayDropoutStack.java
@@ -8,6 +8,7 @@ public class ArrayDropoutStack<T> implements DropoutStackInterface<T>
 
 	private final int DEFAULT_CAPACITY = 3;
 	private int top, bot, count;
+	private int capacity;
 	private T[] stack;
 	/**
 	* Creates an empty queue using the default capacity.
@@ -16,6 +17,7 @@ public class ArrayDropoutStack<T> implements DropoutStackInterface<T>
 	{
 	top = bot = count = 0;
 	stack = (T[]) (new Object[DEFAULT_CAPACITY]); 
+	capacity = 3;
 	}
 	/**
 	* Creates an empty queue using the specified capacity.
@@ -27,6 +29,7 @@ public class ArrayDropoutStack<T> implements DropoutStackInterface<T>
 	{
 	top = bot = count = 0;
 	stack = ( (T[])(new Object[initialCapacity]) );
+	capacity = initialCapacity;
 	}
 	@Override
 	public T pop() throws StackUnderflowException
@@ -53,14 +56,51 @@ public class ArrayDropoutStack<T> implements DropoutStackInterface<T>
 	public void push(T elem)
 	{
 		// TODO Auto-generated method stub
-		if (size() == stack.length){
-			stack[bot] = null;
+//		if (size() == stack.length){
+//			bot = top - (capacity - 1);
+//			
+//			
+//		
+//			
+//			stack[top] = elem;
+//			
+//		}
+		System.out.println(stack[0]);
+		//System.out.println(top);
+		//if(top != capacity){
+			top++;
+//	}
+		if (top >= 2 && capacity == 100000){
+			System.out.println("is called");
+			stack[0] = stack[1];
+			stack[1] = stack[2];
+			stack[2] = elem;
+			
 		}
-		top++;
+//		if(top == 3){
+//			top = 0;
+//		}
+		
+//		if(top > capacity){
+////		for(int i = 0; i < capacity; i++){
+////			
+////			stack[i-1] = stack[i];
+////		}
+		//bot++;
+		//}
+	//	stack[top + 1] = stack[bot];
+	//if(top < 3){
 		stack[top] = elem;
+//	}
 		
-		
+	
+	
 	}
+		
+		
+		
+		
+	
 
 	@Override
 	public boolean isEmpty()
@@ -89,6 +129,10 @@ public class ArrayDropoutStack<T> implements DropoutStackInterface<T>
 		for (int index=0; index < stack.length; index++)
 		larger[index] = stack[index];
 		stack = larger;
+		capacity = newCapacity;
 	}
-
+	@Override
+	public T getStack(int stackNumber){
+		return stack[stackNumber];	
+	}
 }
